f3295efdabcd10a30ddb637efa625424613026bb
Agatha Ambrose <agathambrose@gmail.com>
2026-02-08 15:09:27 +0000

    chore: drop build artifacts and ignore caches

--- apps/salon-admin/.vite/deps_temp_2c64a994/react-dom.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-M67C5VP7.js";
-import "./chunk-7375RAIT.js";
-import "./chunk-WOOG5QLI.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
--- apps/salon-admin/.vite/deps_temp_b6feac30/react_jsx-runtime.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-FYTI5SDZ.js";
-import "./chunk-7375RAIT.js";
-import "./chunk-WOOG5QLI.js";
-export default require_jsx_runtime();
-//# sourceMappingURL=react_jsx-runtime.js.map
